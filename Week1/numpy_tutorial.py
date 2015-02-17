bffc910dd8a4972ac95661d4bf6fd0eb55cc2a87
Jesse Hu <huyouare@gmail.com>
2015-02-16 23:38:58 -0500

    Finish numpy tutorial

--- Week1/numpy_tutorial.py
+++ Week1/numpy_tutorial.py
@@ -11,3 +11,206 @@ print a
 b = np.array([[1,2,3],[4,5,6]]) # Rank 2 array
 print b.shape
 print b[0, 0], b[0, 1], b[1, 0]
+
+a = np.zeros((2,2))
+print a
+
+b = np.ones((1,2))
+print b
+
+c = np.full((2,2), 7)
+print c
+
+d = np.eye(2)
+print d
+
+e = np.random.random((2,2))
+print e
+
+a = np.array([[1,2,3,4],
+              [5,6,7,8],
+              [9,10,11,12]])
+b = a[:2, 1:3]
+
+print a[0, 1]
+b[0, 0] = 77
+print a[0, 1]
+
+import numpy as np
+
+# Create the following rank 2 array with shape (3, 4)
+# [[ 1  2  3  4]
+#  [ 5  6  7  8]
+#  [ 9 10 11 12]]
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+# Use slicing to pull out the subarray consisting of the first 2 rows
+# and columns 1 and 2; b is the following array of shape (2, 2):
+# [[2 3]
+#  [6 7]]
+b = a[:2, 1:3]
+
+# A slice of an array is a view into the same data, so modifying it
+# will modify the original array.
+print a[0, 1]   # Prints "2"
+b[0, 0] = 77    # b[0, 0] is the same piece of data as a[0, 1]
+print a[0, 1]   # Prints "77"
+
+
+a = np.array([[1,2,3,4], [5,6,7,8], [9,10,11,12]])
+
+# Two ways of accessing the data in the middle row of the array.
+# Mixing integer indexing with slices yields an array of lower rank,
+# while using only slices yields an array of the same rank as the
+# original array:
+row_r1 = a[1, :]    # Rank 1 view of the second row of a  
+row_r2 = a[1:2, :]  # Rank 2 view of the second row of a
+print row_r1, row_r1.shape  # Prints "[5 6 7 8] (4,)"
+print row_r2, row_r2.shape  # Prints "[[5 6 7 8]] (1, 4)"
+
+# We can make the same distinction when accessing columns of an array:
+col_r1 = a[:, 1]
+col_r2 = a[:, 1:2]
+print col_r1, col_r1.shape  # Prints "[ 2  6 10] (3,)"
+print col_r2, col_r2.shape  # Prints "[[ 2]
+                            #          [ 6]
+                            #          [10]] (3, 1)"
+
+a = np.array([[1,2], [3, 4], [5, 6]])
+
+# An example of integer array indexing.
+# The returned array will have shape (3,) and 
+print a[[0, 1, 2], [0, 1, 0]]  # Prints "[1 4 5]"
+
+# The above example of integer array indexing is equivalent to this:
+print np.array([a[0, 0], a[1, 1], a[2, 0]])  # Prints "[1 4 5]"
+
+
+# When using integer array indexing, you can reuse the same
+# element from the source array:
+print a[[0, 0], [1, 1]]  # Prints "[2 2]"
+
+# Equivalent to the previous integer array indexing example
+print np.array([a[0, 1], a[0, 1]])  # Prints "[2 2]"
+
+a = np.array([[1,2], [3, 4], [5, 6]])
+
+bool_idx = (a > 2)  # Find the elements of a that are bigger than 2;
+                    # this returns a numpy array of Booleans of the same
+                    # shape as a, where each slot of bool_idx tells
+                    # whether that element of a is > 2.
+
+print bool_idx      # Prints "[[False False]
+                    #          [ True  True]
+                    #          [ True  True]]"
+
+# We use boolean array indexing to construct a rank 1 array
+# consisting of the elements of a corresponding to the True values
+# of bool_idx
+print a[bool_idx]  # Prints "[3 4 5 6]"
+
+# We can do all of the above in a single concise statement:
+print a[a > 2]     # Prints "[3 4 5 6]"
+
+
+x = np.array([1, 2])  # Let numpy choose the datatype
+print x.dtype         # Prints "int64"
+
+x = np.array([1.0, 2.0])  # Let numpy choose the datatype
+print x.dtype             # Prints "float64"
+
+x = np.array([1, 2], dtype=np.int64)  # Force a particular datatype
+print x.dtype                         # Prints "int64"
+
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+# Elementwise sum; both produce the array
+# [[ 6.0  8.0]
+#  [10.0 12.0]]
+print x + y
+print np.add(x, y)
+
+# Elementwise difference; both produce the array
+# [[-4.0 -4.0]
+#  [-4.0 -4.0]]
+print x - y
+print np.subtract(x, y)
+
+# Elementwise product; both produce the array
+# [[ 5.0 12.0]
+#  [21.0 32.0]]
+print x * y
+print np.multiply(x, y)
+
+# Elementwise division; both produce the array
+# [[ 0.2         0.33333333]
+#  [ 0.42857143  0.5       ]]
+print x / y
+print np.divide(x, y)
+
+# Elementwise square root; produces the array
+# [[ 1.          1.41421356]
+#  [ 1.73205081  2.        ]]
+print np.sqrt(x)
+
+### * in numpy is ELEMENTWISE MULTIPLICATION
+### use np.dot(v, w) or v.dot(w) for inner product
+
+x = np.array([[1,2],[3,4]])
+y = np.array([[5,6],[7,8]])
+
+v = np.array([9,10])
+w = np.array([11, 12])
+
+# Inner product of vectors; both produce 219
+print v.dot(w)
+print np.dot(v, w)
+
+# Matrix / vector product; both produce the rank 1 array [29 67]
+print x.dot(v)
+print np.dot(x, v)
+
+# Matrix / matrix product; both produce the rank 2 array
+# [[19 22]
+#  [43 50]]
+print x.dot(y)
+print np.dot(x, y)
+
+x = np.array([[1,2],[3,4]])
+
+print np.sum(x)  # Compute sum of all elements; prints "10"
+print np.sum(x, axis=0)  # Compute sum of each column; prints "[4 6]"
+print np.sum(x, axis=1)  # Compute sum of each row; prints "[3 7]"
+
+x = np.array([[1,2], [3,4]])
+print x    # Prints "[[1 2]
+           #          [3 4]]"
+print x.T  # Prints "[[1 3]
+           #          [2 4]]"
+
+# Note that taking the transpose of a rank 1 array does nothing:
+v = np.array([1,2,3])
+print v    # Prints "[1 2 3]"
+print v.T  # Prints "[1 2 3]"
+
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+vv = np.tile(v, (4, 1))  # Stack 4 copies of v on top of each other
+print vv                 # Prints "[[1 0 1]
+                         #          [1 0 1]
+                         #          [1 0 1]
+                         #          [1 0 1]]"
+y = x + vv  # Add x and vv elementwise
+print y  # Prints "[[ 2  2  4
+         #          [ 5  5  7]
+         #          [ 8  8 10]
+         #          [11 11 13]]"
+
+x = np.array([[1,2,3], [4,5,6], [7,8,9], [10, 11, 12]])
+v = np.array([1, 0, 1])
+y = x + v  # Add v to each row of x using broadcasting
+print y  # Prints "[[ 2  2  4]
+         #          [ 5  5  7]
+         #          [ 8  8 10]
+         #          [11 11 13]]"
