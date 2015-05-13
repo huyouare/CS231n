9b8ca95e0ba065153ec96f3c62c9f31665184adf
Jesse Hu <huyouare@gmail.com>
2015-05-13 19:10:26 -0400

    Finish SGD for Linear SVM

--- assignment1/cs231n/classifiers/linear_classifier.py
+++ assignment1/cs231n/classifiers/linear_classifier.py
@@ -92,7 +92,8 @@ class LinearClassifier:
     # TODO:                                                                   #
     # Implement this method. Store the predicted labels in y_pred.            #
     ###########################################################################
-    pass
+    scores = np.dot(self.W, X)
+    y_pred = scores.argmax(axis=0)
     ###########################################################################
     #                           END OF YOUR CODE                              #
     ###########################################################################
