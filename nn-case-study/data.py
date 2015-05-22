907b5f630d646aa6bf67a5af93aa41078b034d1f
Jesse Hu <huyouare@gmail.com>
2015-05-21 21:39:08 -0400

    Add nn implementation for comparison

--- nn-case-study/data.py
+++ nn-case-study/data.py
@@ -18,6 +18,7 @@ plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
 plt.show()
 
 ## Train a Linear Classifier
+
 # initialize parameters randomly
 W = 0.01 * np.random.randn(D,K)
 b = np.zeros((1,K))
@@ -31,7 +32,7 @@ num_examples = X.shape[0]
 for i in xrange(200):
 
   # evaluate class scores, [N x K]
-  scores = np.dot(X, W) + b 
+  scores = np.dot(X, W) + b
 
   # compute the class probabilities
   exp_scores = np.exp(scores)
@@ -59,3 +60,74 @@ for i in xrange(200):
   # perform a parameter update
   W += -step_size * dW
   b += -step_size * db
+
+# evaluate training set accuracy
+scores = np.dot(X, W) + b
+predicted_class = np.argmax(scores, axis=1)
+print 'training accuracy: %.2f' % (np.mean(predicted_class == y))
+
+## Train a neural network
+
+# initialize parameters randomly
+h = 100 # size of hidden layer
+W = 0.01 * np.random.randn(D,h)
+b = np.zeros((1,h))
+W2 = 0.01 * np.random.randn(h,K)
+b2 = np.zeros((1,K))
+
+# some hyperparameters
+step_size = 1e-0
+reg = 1e-3 # regularization strength
+
+# gradient descent loop
+num_examples = X.shape[0]
+for i in xrange(10000):
+
+  # evaluate class scores, [N x K]
+  hidden_layer = np.maximum(0, np.dot(X, W) + b) # note, ReLU activation
+  scores = np.dot(hidden_layer, W2) + b2
+
+  # compute the class probabilities
+  exp_scores = np.exp(scores)
+  probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
+
+  # compute the loss: average cross-entropy loss and regularization
+  corect_logprobs = -np.log(probs[range(num_examples),y])
+  data_loss = np.sum(corect_logprobs)/num_examples
+  reg_loss = 0.5*reg*np.sum(W*W) + 0.5*reg*np.sum(W2*W2)
+  loss = data_loss + reg_loss
+  if i % 1000 == 0:
+    print "iteration %d: loss %f" % (i, loss)
+
+  # compute the gradient on scores
+  dscores = probs
+  dscores[range(num_examples),y] -= 1
+  dscores /= num_examples
+
+  # backpropate the gradient to the parameters
+  # first backprop into parameters W2 and b2
+  dW2 = np.dot(hidden_layer.T, dscores)
+  db2 = np.sum(dscores, axis=0, keepdims=True)
+  # next backprop into hidden layer
+  dhidden = np.dot(dscores, W2.T)
+  # backprop the ReLU non-linearity
+  dhidden[hidden_layer <= 0] = 0
+  # finally into W,b
+  dW = np.dot(X.T, dhidden)
+  db = np.sum(dhidden, axis=0, keepdims=True)
+
+  # add regularization gradient contribution
+  dW2 += reg * W2
+  dW += reg * W
+
+  # perform a parameter update
+  W += -step_size * dW
+  b += -step_size * db
+  W2 += -step_size * dW2
+  b2 += -step_size * db2
+
+# evaluate training set accuracy
+hidden_layer = np.maximum(0, np.dot(X, W) + b)
+scores = np.dot(hidden_layer, W2) + b2
+predicted_class = np.argmax(scores, axis=1)
+print 'training accuracy: %.2f' % (np.mean(predicted_class == y))
