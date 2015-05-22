f327100b218ed6fbc7fd8b734999839d36414e57
Jesse Hu <huyouare@gmail.com>
2015-05-21 21:30:31 -0400

    Add example for nn case study

---
+++ nn-case-study/data.py
@@ -0,0 +1,61 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+## Create spiral data
+N = 100 # number of points per class
+D = 2 # dimensionality
+K = 3 # number of classes
+X = np.zeros((N*K,D)) # data matrix (each row = single example)
+y = np.zeros(N*K, dtype='uint8') # class labels
+for j in xrange(K):
+  ix = range(N*j,N*(j+1))
+  r = np.linspace(0.0,1,N) # radius
+  t = np.linspace(j*4,(j+1)*4,N) + np.random.randn(N)*0.2 # theta
+  X[ix] = np.c_[r*np.sin(t), r*np.cos(t)]
+  y[ix] = j
+# lets visualize the data:
+plt.scatter(X[:, 0], X[:, 1], c=y, s=40, cmap=plt.cm.Spectral)
+plt.show()
+
+## Train a Linear Classifier
+# initialize parameters randomly
+W = 0.01 * np.random.randn(D,K)
+b = np.zeros((1,K))
+
+# some hyperparameters
+step_size = 1e-0
+reg = 1e-3 # regularization strength
+
+# gradient descent loop
+num_examples = X.shape[0]
+for i in xrange(200):
+
+  # evaluate class scores, [N x K]
+  scores = np.dot(X, W) + b 
+
+  # compute the class probabilities
+  exp_scores = np.exp(scores)
+  probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True) # [N x K]
+
+  # compute the loss: average cross-entropy loss and regularization
+  corect_logprobs = -np.log(probs[range(num_examples),y])
+  data_loss = np.sum(corect_logprobs)/num_examples
+  reg_loss = 0.5*reg*np.sum(W*W)
+  loss = data_loss + reg_loss
+  if i % 10 == 0:
+    print "iteration %d: loss %f" % (i, loss)
+
+  # compute the gradient on scores
+  dscores = probs
+  dscores[range(num_examples),y] -= 1
+  dscores /= num_examples
+
+  # backpropate the gradient to the parameters (W,b)
+  dW = np.dot(X.T, dscores)
+  db = np.sum(dscores, axis=0, keepdims=True)
+
+  dW += reg*W # regularization gradient
+
+  # perform a parameter update
+  W += -step_size * dW
+  b += -step_size * db
